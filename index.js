76456e36af543ed7ce0e61785299ce1619cbf9ef
Aquib <hossainaquib20@gmail.com>
2022-01-20 02:23:31 +0600

    added second

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { MongoClient} = require('mongodb');
 const ObjectId=require('mongodb').ObjectId;
 require('dotenv').config()
 const app=express()
-const port=4000
+const port=process.env.PORT || 4000
 app.use(express.json())
 app.use(cors())
 
